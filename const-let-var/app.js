f9e88164c01d2d555e2f67000545d38e0d34a3ec
iamrajabli <iamrajabli@outlook.com>
2022-04-29 16:00:00 +0400

    Example 6

--- const-let-var/app.js
+++ const-let-var/app.js
@@ -23,10 +23,21 @@ function codeSchool() {
 
 // Example 4
 let book = 'The call of the wild';
-console.log(book); // Nəticə: The call of the wild
+// console.log(book); // Nəticə: The call of the wild
 book = 'Robin Hood';
 // console.log(book); // Nəticə: Robin Hood
 
 // Example 5
 // let personName = 'Samir';
 // let personName; // Nəticə: Identifier 'personName' has already been declared
+
+// Example 6
+var x = 1;
+console.log(x); // Nəticə: 1
+
+if (x == 1) {
+    var x = 10;
+    console.log(x); // Nəticə: 10
+}
+
+console.log(x); // Nəticə: 10
