e9bb85c58beb9a937ffdc9ca1ae4edad88916f7a
iamrajabli <iamrajabli@outlook.com>
2022-04-29 14:39:28 +0400

    Example 3

--- const-let-var/app.js
+++ const-let-var/app.js
@@ -5,10 +5,18 @@ function developing() {
     let course = 'javascript';
     console.log(course);
 }
-developing();
+// developing();
 
 // Example 2
 function person() {
     let fullName = 'Hikmat Rajabli';
 }
-console.log(fullName); // Nəticə: fullName is not defined
+// console.log(fullName); // Nəticə: fullName is not defined
+
+// Example 3
+let pl = ['js', 'c++', 'c#', 'php'];
+
+function codeSchool() {
+    console.log(pl);
+}
+codeSchool();
