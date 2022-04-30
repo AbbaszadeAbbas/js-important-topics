3bd39e9c8a86c8a07a954925742bb8ee73764580
iamrajabli <iamrajabli@outlook.com>
2022-04-30 16:06:28 +0400

    Example 4

--- functions/app.js
+++ functions/app.js
@@ -7,6 +7,7 @@ function ex1(num) {
 // console.log(ex1(5)); // => 25
 
 // Example 2
+// Hoisting in Declaration
 // console.log(ex2(4)); // => 16
 function ex2(num) {
     return num * num;
@@ -15,6 +16,12 @@ function ex2(num) {
 // Example 3
 // Function expression
 const ex3 = function(num) {
+        return num * num
+    } // console.log(ex3(2)); // => 4
+
+// Example 4
+// Hoisting in Expression
+// console.log(ex4(9)); // => Cannot access 'ex4' before initialization
+const ex4 = function(num) {
     return num * num
 }
-console.log(ex3(2)); // => 4
