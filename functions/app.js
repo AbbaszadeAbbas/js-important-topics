f682494500b5f2f97badd32141309f915f0de339
iamrajabli <iamrajabli@outlook.com>
2022-04-30 09:58:53 +0400

    Example 3

--- functions/app.js
+++ functions/app.js
@@ -7,7 +7,14 @@ function ex1(num) {
 // console.log(ex1(5)); // Nəticə 25
 
 // Example 2
-console.log(ex2(4)); // Nəticə 16
+// console.log(ex2(4)); // Nəticə 16
 function ex2(num) {
     return num * num;
 }
+
+// Example 3
+// Function expression
+const ex3 = function(num) {
+    return num * num
+}
+console.log(ex3(2)); // Nəticə 4
