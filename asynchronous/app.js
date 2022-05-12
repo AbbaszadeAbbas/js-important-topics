5558a8769938b4e0265e4e0af1ee7aff4ee3736d
iamrajabli <iamrajabli@outlook.com>
2022-05-13 01:18:04 +0400

    File organize

--- asynchronous/app.js
+++ asynchronous/app.js
@@ -62,13 +62,13 @@ function task(message) {
     console.log(message);
 }
 
-console.log(1);
+// console.log(1);
 
 setTimeout(() => {
-    console.log(2);
+    // console.log(2);
 }, 1000);
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
 
-task('Process completed!')
+// task('Process completed!')
